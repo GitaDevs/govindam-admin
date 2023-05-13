756b167677ffc2e7699cdb78ca3a24792483c057
aakashsingh-maz <aakashsingh@zype.com>
2023-05-14 00:36:08 +0530

    add all database fields

---
+++ src/api/meal-item/controllers/meal-item.ts
@@ -0,0 +1,7 @@
+/**
+ * meal-item controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::meal-item.meal-item');
---
+++ src/api/stock-inventory/services/stock-inventory.ts
@@ -0,0 +1,7 @@
+/**
+ * stock-inventory service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::stock-inventory.stock-inventory');
